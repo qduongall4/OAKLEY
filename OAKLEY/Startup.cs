24f8bb738e0ddcb55d0b595d5c0f6a6b42ec81ca
qduongall4 <qduong@all4inc.com>
2024-11-18 11:18:05 -0800

    Final

---
+++ OAKLEY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAKLEY.Startup))]
+namespace OAKLEY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
